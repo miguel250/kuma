16b4722b4a0879529863e9a6e8936c5ccf1c1e64
Miguel Perez <miguel@miguelpz.com>
2020-09-13 20:51:08 -0400

    http/server: start listener sooner
    We need to start the listener early so we can return an error if needed.

--- http/server/server.go
+++ http/server/server.go
@@ -11,56 +11,59 @@ import (
 )
 
 type Server struct {
-	Addr    string
-	config  *Config
-	running bool
-	srv     *http.Server
-	stop    chan os.Signal
-	start   chan bool
+	Addr     string
+	config   *Config
+	running  bool
+	srv      *http.Server
+	stop     chan os.Signal
+	start    chan bool
+	listener net.Listener
 }
 
-func (s *Server) Start() {
+func (s *Server) Start() error {
+	err := s.listen()
+	if err != nil {
+		return fmt.Errorf("failed to listen with %w", err)
+	}
+
 	go func() {
-		listener, err := s.Listen()
-
-		if err != nil {
-			log.Printf("Failed to start server with %s", err)
-			signal.Stop(s.stop)
-			close(s.stop)
-			s.running = false
-		}
-
 		s.running = true
 		s.start <- true
 
 		log.Printf("Listening on %s\n", s.Addr)
 
-		if err := s.srv.Serve(listener); err != nil && err != http.ErrServerClosed {
+		if err := s.srv.Serve(s.listener); err != nil && err != http.ErrServerClosed {
 			log.Printf("Failed to start server with %v\n", err)
 			signal.Stop(s.stop)
 			close(s.stop)
 			s.running = false
 		}
 	}()
+	return nil
 }
 
-func (s *Server) Listen() (net.Listener, error) {
+func (s *Server) listen() error {
 	l, err := net.Listen("tcp", s.srv.Addr)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	s.Addr = "http://" + l.Addr().String()
-	return l, nil
+	s.listener = l
+	return nil
 }
 
-func (s *Server) StartAndWait() {
-	s.Start()
-	<-s.stop
-	if s.running {
-		s.Stop()
+func (s *Server) StartAndWait() error {
+	if !s.running {
+		err := s.Start()
+		if err != nil {
+			return err
+		}
 	}
+
+	<-s.stop
+	return s.Stop()
 }
 
 func (s *Server) Stop() error {
@@ -93,6 +96,5 @@ func New(config *Config, handler http.Handler) *Server {
 		config: config,
 		srv:    srv,
 		stop:   stop,
-		start:  start,
-	}
+		start:  start}
 }
